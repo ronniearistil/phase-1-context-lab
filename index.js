aad3af75a7f0b346178223abb2622de279232fc6
Ronnie Aristil <ronnie.aristil@gmail.com>
2024-09-23 08:17:12 -0700

    Complete lab

--- index.js
+++ index.js
@@ -1,13 +1,58 @@
-/* Your Code Here */
-
-/*
- We're giving you this function. Take a look at it, you might see some usage
- that's new and different. That's because we're avoiding a well-known, but
- sneaky bug that we'll cover in the next few lessons!
-
- As a result, the lessons for this function will pass *and* it will be available
- for you to use if you need it!
- */
+function createEmployeeRecord(employeeData) {
+    return {
+      firstName: employeeData[0],
+      familyName: employeeData[1],
+      title: employeeData[2],
+      payPerHour: employeeData[3],
+      timeInEvents: [],
+      timeOutEvents: []
+    };
+  }
+  
+  function createEmployeeRecords(employeeDataArray) {
+    return employeeDataArray.map(createEmployeeRecord);
+  }
+  
+  function createTimeInEvent(dateTime) {
+    let [date, hour] = dateTime.split(' ');
+    this.timeInEvents.push({
+      type: "TimeIn",
+      hour: parseInt(hour, 10),
+      date: date
+    });
+    return this;
+  }
+  
+  function createTimeOutEvent(dateTime) {
+    let [date, hour] = dateTime.split(' ');
+    this.timeOutEvents.push({
+      type: "TimeOut",
+      hour: parseInt(hour, 10),
+      date: date
+    });
+    return this;
+  }
+  
+  function hoursWorkedOnDate(specificDate) {
+    let timeIn = this.timeInEvents.find(event => event.date === specificDate);
+    let timeOut = this.timeOutEvents.find(event => event.date === specificDate);
+    return (timeOut.hour - timeIn.hour) / 100;
+  }
+  
+  function wagesEarnedOnDate(specificDate) {
+    let hoursWorked = this.hoursWorkedOnDate(specificDate);
+    return hoursWorked * this.payPerHour;
+  }
+  
+  function findEmployeeByFirstName(srcArray, firstName) {
+    return srcArray.find(employee => employee.firstName === firstName);
+  }
+  
+  function calculatePayroll(employeeRecords) {
+    return employeeRecords.reduce((totalPayroll, employee) => {
+      return totalPayroll + employee.allWagesFor();
+    }, 0);
+  }  
 
 const allWagesFor = function () {
     const eligibleDates = this.timeInEvents.map(function (e) {
